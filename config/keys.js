1962eff8d7b39d93a641122cdaa977cf3825ccdf
alanpv71 <alanpv1996@gmail.com>
2019-09-26 19:46:09 +0530

    login end points ,form validation, routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : "mongodb+srv://admin:admin@getmybus-a9dlh.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI : "mongodb+srv://admin:admin@getmybus-a9dlh.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 }
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require ('express')
 const bodyParser = require ('body-parser')
 const mongoose = require ('mongoose')
 
+const passport = require("passport");
+const users = require("./routes/api/users");
+
 const app = express();
 
 //body parser
@@ -16,6 +19,13 @@ const db = require("./config/keys").mongoURI;
 //mongodb connection
 mongoose.connect(db, {useNewUrlParser : true} ).then(console.log("MongoDB Connected Successfully")).catch(err => console.log(err));
 
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 //port setup
 const port = process.env.PORT || 5000 ;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
