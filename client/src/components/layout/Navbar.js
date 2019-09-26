3f2422fd02bc61c9cdb175fc5cd295bc233dddb7
alanpv71 <alanpv1996@gmail.com>
2019-09-26 22:22:40 +0530

    frnt end

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper orange">
+          <div class="brand-logo"> <Link
+              to="/"
+              style={{
+                fontFamily: "monospace"
+              }}
+              className="col s5 brand-logo left white-text">
+            Logo</Link></div>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">Book Now</a></li>
+      
+        <li><a href="collapsible.html">About</a></li>
+      </ul>
+            <Link
+              to="/"
+              style={{
+                fontFamily: "monospace"
+              }}
+              className="col s5 brand-logo left orange-text">
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Navbar;
