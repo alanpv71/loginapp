9b4cc4b130f547db8f0a776d9b8caa3e3d1f4993
alanpv71 <alanpv1996@gmail.com>
2019-09-28 23:19:04 +0530

    minor fixes in register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,14 +1,34 @@
 import React, {Component}  from "react";
-
+import axios from 'axios';
 
 class Register extends Component {
-    
-    handleSubmit(e){
+    state = {
+        username  :"",
+        email  : "",
+        password : "",
+        error : {}
+    };
+    handleChange = e =>{
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+    handleSubmit= e =>{
         e.preventDefault();
+        const newUser = {
+            username:this.state.username,
+            email:this.state.email,
+            password:this.state.password
+        };
+        axios.post('/api/users/register',newUser).then(res=>this.props.history.push('/login')).catch(err=> this.setState({
+            error : err.response.data
+        })
+        );
          };
      
    
     render(){
+        const {username,email,password,error} = this.state;
         return (
             
             <React.Fragment>
@@ -18,15 +38,15 @@ class Register extends Component {
                             <h2 className="mt-5 text-center mb-0">Register Here</h2>
                             <form onSubmit={this.handleSubmit}>
                                 <div className="form-group">
-                                    <input type="text" name="username" className="form-control" placeholder="Username"/>
+                                    <input type="text" name="username" value ={username} onChange={this.handleChange} className={error.type ==="username" ? "is-invalid form-control" : "form-control"} placeholder="Username"/><span className="invalid-feedback">{error.msg}</span>
     
                                 </div>
                                 <div className="form-group">
-                                    <input type="email" name="email" className="form-control" placeholder="Email"/>
+                                    <input type="email" name="email" value = {email} onChange={this.handleChange} className={error.type ==="email" ? "is-invalid form-control" : "form-control"} placeholder="Email"/><span className="invalid-feedback">{error.msg}</span>
                                     
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" name="password" className="form-control" placeholder="Password"/>
+                                    <input type="password" name="password" value={password} onChange={this.handleChange} className={error.type ==="password" ? "is-invalid form-control" : "form-control"} placeholder="Password"/><span className="invalid-feedback">{error.msg}</span>
                                     </div>  
                                     <input type="submit" className="btn btn-primary justify-content-center d-flex w-100" value="Register"/>
                                 
