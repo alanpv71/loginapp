99c9bc63d4a4719ac2cff6c537e9329e4f6ac2ab
alanpv71 <alanpv1996@gmail.com>
2019-10-05 21:23:55 +0530

    Added navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
-
+import busanim from './booknow-anim.gif';
 import Register from "./components/auth/Register";
 import Login from './components/auth/Login';
 import Dashboard from './components/layouts/Dashboard';
@@ -11,11 +11,12 @@ class App extends Component {
     return (
       
       <Router>
-    <div className="Header"></div>
+   hiiiiii
+   <Link to="/login">LKoya</Link>
     <Switch>
       <Route  path="/dashboard" component = {Dashboard}></Route>
-      <Route exact path ="/register" component= { Register }></Route>
-      <Route path ="/login" component= { Login }></Route>
+      <Route  path ="/register" component= { Register }></Route>
+      <Route  path ="/login" component= { Login }></Route>
     </Switch>
     
     </Router>
